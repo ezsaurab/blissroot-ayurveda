5814a9bbfc7e99b9377054797c876c2cf53abb83
Saurabh Chauhan <your_email@example.com>
2026-09-05 16:43:57 +0530

    chore: setup production build and MPA config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        cart: resolve(__dirname, 'cart.html'),
+        orderTracking: resolve(__dirname, 'order-tracking.html'),
+        chyawanprash: resolve(__dirname, 'chyawanprash.html'),
+        amlaMurabba: resolve(__dirname, 'amla-murabba.html'),
+        herbalLipBalm: resolve(__dirname, 'herbal-lip-balm.html')
+      }
+    }
+  },
   server: {
     port: 5173,
     proxy: {
